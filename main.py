75a1af91d371d3a54f8d57d2711e692e749fcd36
skile <skilet.e@yandex.ru>
2023-09-09 17:53:27 +0400

    in progres

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() # инициализируем часы
 
 pygame.init() # инициализация игры
 
@@ -11,13 +11,13 @@ screen.fill((97, 178, 199))
 
 
 icon = pygame.image.load('image/free-icon-abstract-shape-6604311.png') # подгружаем иконку
-pygame.display.set_icon(icon)
+pygame.display.set_icon(icon) # устанавливаем эту иконку для игры
 
 bg = pygame.image.load('image/1641565350_33-www-funnyart-club-p-foni-dlya-platformerov-34.png') # создаем зандий фон
 
-player_speed = 50
-player_x = 200
-player_y = 100
+player_speed = 50 # скорость движения персонажа
+player_x = 200 # координаты по x
+player_y = 100 # координаты по Y
 
 walk_right = [
     pygame.image.load("image/move/right1.png"),
@@ -25,7 +25,7 @@ walk_right = [
     pygame.image.load("image/move/right3.png"), # движение игрока вправо
     pygame.image.load("image/move/right4.png"),
     pygame.image.load("image/move/right5.png"),
-    pygame.image.load("image/move/right6.png"),
+
 
 ]
 walk_left = [
@@ -34,42 +34,40 @@ walk_left = [
     pygame.image.load("image/move/left3.png"),
     pygame.image.load("image/move/left4.png"),# движение игрока влево
     pygame.image.load("image/move/left5.png"),
-    pygame.image.load("image/move/left6.png"),
+
 
 ]
 
-player = pygame.image.load("image/move/Staite.png")
-wk = 0
-bg_x = 0
-is_jump = False
-up_jump = 7
 
-while True:
-    clock.tick(10)
-    screen.blit(bg,(0,0))
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
+wk = 0 # счетчик для движения персонажа , он меняется на 1 за тик и сменяет картинки
+bg_x = 0 # движение заднего фона
+is_jump = False # переменная для отслеживания прыжка
+up_jump = 7 # высота прыжка
+
+while True: # цикл для основной игры
+    clock.tick(15) # обмен кадров
+    screen.blit(bg,(0,0)) # создание заднего фона в координатах 0
+    keys = pygame.key.get_pressed() # отслеживание нажатия
+    if keys[pygame.K_d]: # проверка если нажали d то запускаем анимацию движения вправо
         screen.blit(walk_right[wk],(player_x,player_y))
-    elif keys[pygame.K_a]:
+    elif keys[pygame.K_a]: # если нажали А то запускаем движение анимации влево
         screen.blit(walk_left[wk], (player_x, player_y))
-    else:
-        screen.blit(player, (player_x, 100))
+    else: # в противном случае анимация остановки
+        screen.blit(walk_right[wk], (player_x, player_y))
 
-    if keys[pygame.K_LSHIFT]:
+    if keys[pygame.K_LSHIFT]: # если нажали шифт то ускорение в 2 раза
         player_speed = 100
     else:
         player_speed = 50
-    if not is_jump:
+
+
+    if not is_jump: # прописывание логики работы прыжка
         if keys[pygame.K_SPACE]:
             is_jump = True
-
-
-
     else:
         if up_jump >= -7:
             if up_jump > 0:
                 player_y -= (up_jump ** 2 )/2
-
             else:
                 player_y += (up_jump ** 2 )/2
             up_jump -= 1
@@ -78,20 +76,23 @@ while True:
             up_jump = 7
 
 
-    if keys[pygame.K_a] and player_x > 0:
+    if keys[pygame.K_a] and player_x > 0: # передвижение персонажа по экрану с ограничением движения
         player_x -= player_speed
 
     elif keys[pygame.K_d] and player_x < 800:
         player_x += player_speed
 
-    if wk == 5:
+    elif keys[pygame.K_w] and player_y > 0:
+        player_y -= player_speed
+    elif keys[pygame.K_s] and player_y < 420:
+        player_y += player_speed
+
+    if wk == 4: # обновления анимации движения и обнуление если мы выходим за диапазон
         wk = 0
     else:
         wk += 1
-    pygame.display.update()
+    pygame.display.update() # обновление событий на экране
 
-
-    bg_x -= 10
     for event in pygame.event.get():# получаем список всех возможных событий
         if event.type == pygame.QUIT:# проверяем если тип переменной ивент == выходу из игры
             pygame.quit() # закрываем окно и выключаем цикл
